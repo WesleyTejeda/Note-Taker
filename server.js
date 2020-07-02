e874b1f7ef40cf0d59446161773452980d8536ed
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-02 14:46:48 -0400

    Added method conditional statements

--- server.js
+++ server.js
@@ -6,12 +6,74 @@ var PORT = 8080;
 function handleRequest(req, res) {
     var path = req.url;
 
-    fs.readFile(__dirname+path, function(err, data){
-        if (err)
-            throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    })
+    if (req.method == 'POST') {
+        // Saving the request data as a variable
+        var requestData = "";
+
+        // When the server receives data...
+        req.on("data", function(data) {
+
+            // Add it to requestData.
+            requestData += data;
+        });
+
+        // When the request has ended...
+        req.on("end", function() {
+
+            // Log (server-side) the request method, as well as the data received!
+            console.log("You did a", req.method, "with the data:\n", requestData);
+            res.end();
+        });
+    }
+
+    if (req.method == 'GET') {
+        // Saving the request data as a variable
+        var requestData = "";
+
+        // When the server receives data...
+        req.on("data", function(data) {
+
+            // Add it to requestData.
+            requestData += data;
+        });
+
+        // When the request has ended...
+        req.on("end", function() {
+
+            // Log (server-side) the request method, as well as the data received!
+            console.log("You did a", req.method, "with the data:\n", requestData);
+            res.end();
+        });
+    }
+
+    if (req.method == 'DELETE') {
+        // Saving the request data as a variable
+        var requestData = "";
+
+        // When the server receives data...
+        req.on("data", function(data) {
+
+            // Add it to requestData.
+            requestData += data;
+        });
+
+        // When the request has ended...
+        req.on("end", function() {
+
+            // Log (server-side) the request method, as well as the data received!
+            console.log("You did a", req.method, "with the data:\n", requestData);
+            res.end();
+        });
+    }
+    
+    switch (path) {
+        case "/notes":
+            return displayNotes();
+        case "*":
+            return displayIndex();
+        default:
+            return displayErr404();
+    } 
 }
 
 var server = http.createServer(handleRequest);
@@ -20,3 +82,14 @@ server.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
   
+function displayNotes() {
+    //Write out code to display notes.html
+}
+
+function displayIndex() {
+    //Write out code to display index.html
+}
+
+function displayErr404(){
+    //Write out code to display 404 page
+}

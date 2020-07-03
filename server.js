92a1af28e5191fac71df9dd585358939c6d4acc5
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-03 03:17:47 -0400

    Updated id method, cleaned lines of code

--- server.js
+++ server.js
@@ -1,67 +1,3 @@
-// const http = require("http");
-// const fs = require("fs");
-
-// let PORT = 8080;
-
-// function handleRequest(req, res) {
-//     let path = req.url;
-//     console.log(req.method);
-//     if(path === "/api/notes"){
-//         if (req.method === "GET"){
-
-//         }
-    
-//         if (req.method === "POST"){
-    
-//         }
-//         if (req.method === "DELETE"){
-            
-//         }
-//     }
-   
-//     switch (path) {
-//         case "/notes":
-//             return displayNotes(res);
-//         case "/":
-//             return displayIndex(res);
-//         default:
-//             return displayErr404(res);
-//     } 
-// }
-
-// var server = http.createServer(handleRequest);
-
-// server.listen(PORT, function() {
-//     console.log("Server listening on: http://localhost:" + PORT);
-//   });
-  
-// function displayNotes(res) {
-//     //Write out code to display notes.html
-//     fs.readFile(__dirname+"/public/notes.html", function(err, data){
-//         if (err)
-//             throw err;
-//         res.writeHead(200, { "Content-Type": "text/html" });
-//         res.end(data);
-//     })
-// }
-
-// function displayIndex(res) {
-//     //Write out code to display index.html
-//     fs.readFile(__dirname+"/public/index.html", function(err, data){
-//         if (err)
-//             throw err;
-//         res.writeHead(200, { "Content-Type": "text/html" });
-//         res.end(data);
-//     })
-    
-// }
-
-// function displayErr404(res){
-//     //Write out code to display 404 page
-//     var myHTML = "<h1>404</h1>";
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.end(myHTML);
-// }
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
@@ -69,14 +5,12 @@ const path = require('path');
 const router = express.Router();
 let PORT = 8080;
 const app = express();
+app.listen(PORT);
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-
-// router.use((req, res, next) => {
-//     next();
-// })
 
 
 app.get("/", (req, res) => {
@@ -87,24 +21,25 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
-//Returns JSON 
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname + '/db/db.json'));
 });
 
 app.post("/api/notes", (req, res) => {
-    let newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: Math.floor(Math.random()*999999999)
-    };
+    let id = 0;
     fs.readFile(__dirname+'/db/db.json', 'utf-8', (err, data) => {
         if (err)
             res.status(500).json({message: "File not found."});
         let oldJson = JSON.parse(data);
-        console.log(oldJson[0], typeof(oldJson));
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+        };
         oldJson.push(newNote);
-
+        oldJson.forEach(object => {
+            object.id = id;
+            id++;
+        })
         fs.writeFile(__dirname+'/db/db.json', JSON.stringify(oldJson), (err) => {
             if (err)
                 res.status(500).json({message: "Error."});
@@ -129,7 +64,10 @@ app.delete("/api/notes/:id", (req, res) => {
         console.log(objToDel);
         let indexDel = oldJson.indexOf(objToDel);
         console.log(indexDel);
-        oldJson.splice(indexDel, indexDel+1);
+        if(indexDel === 0)
+            oldJson.shift();
+        else oldJson.splice(indexDel, 1);
+        console.log(oldJson);
         fs.writeFile(__dirname+'/db/db.json', JSON.stringify(oldJson), (err) => {
             if (err)
                 res.status(500).json({message: "Error."});
@@ -137,6 +75,3 @@ app.delete("/api/notes/:id", (req, res) => {
         })
     });
 })
-// app.use(router);
-
-app.listen(PORT);
